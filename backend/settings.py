259e95aabe7c2d2922cc44fec833647668fe47b7
Behzad Javadian <behzadjavadian49@gmail.com>
2025-03-11 20:59:24 +0100

    add and change some code for Bookmarks app

--- backend/settings.py
+++ backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'collaborations',
     'comments',
     'ratings',
+    'bookmarks',
 ]
 
 MIDDLEWARE = [
---
+++ bookmarks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from users.models import UserProfile
+from podcasts.models import Podcast
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    expert = models.ForeignKey(UserProfile, on_delete=models.CASCADE, blank=True, null=True, related_name="bookmarked_experts")
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} bookmarked {self.expert or self.podcast}"
