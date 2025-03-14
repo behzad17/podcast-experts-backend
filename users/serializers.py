0b8838d741b95f15b46ca208833822f33efd581e
Behzad Javadian <behzadjavadian49@gmail.com>
2025-03-14 16:14:59 +0100

    fix codes in serializers.py users

--- users/serializers.py
+++ users/serializers.py
@@ -9,6 +9,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'user_type')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
