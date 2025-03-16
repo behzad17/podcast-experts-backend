469a1b87529f8ec3718be781b9e8d04a14a171f0
Behzad Javadian <behzadjavadian49@gmail.com>
2025-03-16 16:44:38 +0100

    Add logout functionality to Navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
+  const isAuthenticated = localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
     <Navbar bg="primary" variant="dark" expand="lg" className="shadow">
       <Container>
-        <Navbar.Brand as={Link} to="/">🎙 Podcast Platform</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          🎙 Podcast Platform
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/experts">Experts</Nav.Link>
-            <Nav.Link as={Link} to="/podcasts">Podcasts</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/experts">
+              Experts
+            </Nav.Link>
+            <Nav.Link as={Link} to="/podcasts">
+              Podcasts
+            </Nav.Link>
+            {!isAuthenticated ? (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Button
+                  variant="outline-light"
+                  onClick={handleLogout}
+                  className="ms-2"
+                >
+                  Logout
+                </Button>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -23,4 +61,3 @@ const NavigationBar = () => {
 };
 
 export default NavigationBar;
-
