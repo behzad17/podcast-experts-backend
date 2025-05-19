650069983ffd187bb98b31be1b39547e954c13d1
Behzad Javadian <behzadjavadian49@gmail.com>
2025-05-19 21:34:37 +0200

    Fix: Recreate MessageList component

---
+++ frontend/src/components/messages/MessageList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MessageList = () => {
+  return (
+    <div>
+      <h2>Messages</h2>
+      {/* Message list content will go here */}
+    </div>
+  );
+};
+
+export default MessageList;
