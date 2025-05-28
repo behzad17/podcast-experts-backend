ab1b579b72e0c1daf209e3aa732dff5899e46509
Behzad Javadian <behzadjavadian49@gmail.com>
2025-05-28 13:46:01 +0200

    Use built frontend and simplified startup

--- build_and_serve.sh
+++ build_and_serve.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-# Build frontend
-cd frontend
-npm install
-npm run build
-cd ..
-
-# Collect static files
 python manage.py collectstatic --noinput
-
-# Run the server
 gunicorn backend.wsgi --log-file -
-
