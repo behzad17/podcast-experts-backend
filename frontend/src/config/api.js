76e268d8043ae7d126f93af05d5c6fc33e94bf41
Behzad Javadian <behzadjavadian49@gmail.com>
2025-05-29 14:00:08 +0200

    Update frontend API URL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://podcast-backend-4e5439705bd3.herokuapp.com";
 
 // Create axios instance with default config
 const api = axios.create({
